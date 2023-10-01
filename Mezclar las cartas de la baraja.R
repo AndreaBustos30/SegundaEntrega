7640f7413af6f0dec22361522eceb9bafa794aa1
AndreaBustos30 <andreabustosal@gmail.com>
2023-09-30 20:32:30 -0500

    se crea el código en R para mezclar las cartas de una baraja

---
+++ Mezclar las cartas de la baraja.R
@@ -0,0 +1,8 @@
+# Crear una baraja de 52 cartas
+baraja <- 1:52
+
+# Mezclar las cartas
+baraja_mezclada <- sample(baraja)
+
+# Mostrar la baraja mezclada
+cat("Baraja mezclada:", baraja_mezclada, "\n")
