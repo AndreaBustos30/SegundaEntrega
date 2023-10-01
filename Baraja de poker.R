a3ad7301f746403a59e78972f9de233016c87556
AndreaBustos30 <andreabustosal@gmail.com>
2023-09-30 21:27:43 -0500

    se implemento un código para crear una baraja de poker

---
+++ Baraja de poker.R
@@ -0,0 +1,11 @@
+# Crear una baraja de póker
+baraja <- c("2♠", "3♠", "4♠", "5♠", "6♠", "7♠", "8♠", "9♠", "10♠", "J♠", "Q♠", "K♠", "A♠",
+            "2♥", "3♥", "4♥", "5♥", "6♥", "7♥", "8♥", "9♥", "10♥", "J♥", "Q♥", "K♥", "A♥",
+            "2♦", "3♦", "4♦", "5♦", "6♦", "7♦", "8♦", "9♦", "10♦", "J♦", "Q♦", "K♦", "A♦",
+            "2♣", "3♣", "4♣", "5♣", "6♣", "7♣", "8♣", "9♣", "10♣", "J♣", "Q♣", "K♣", "A♣")
+
+# Mezclar la baraja
+baraja_mezclada <- sample(baraja)
+
+# Mostrar la baraja mezclada
+cat("Baraja de póker mezclada:\n", baraja_mezclada, "\n")
