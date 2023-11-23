9621c4e565705f70f6b3bf387eebb712e124003b
AndreaBustos30 <143854421+AndreaBustos30@users.noreply.github.com>
2023-11-22 20:21:47 -0500

    Delete Mezclar las cartas de la baraja.R

--- Mezclar las cartas de la baraja.R
+++
@@ -1,8 +0,0 @@
-# Crear una baraja de 52 cartas
-baraja <- 1:52
-
-# Mezclar las cartas
-baraja_mezclada <- sample(baraja)
-
-# Mostrar la baraja mezclada
-cat("Baraja mezclada:", baraja_mezclada, "\n")
