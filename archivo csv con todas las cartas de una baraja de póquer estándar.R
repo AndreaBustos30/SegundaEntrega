fc6ac1d98a0ff3fe216083e72601538dd630d3d7
AndreaBustos30 <143854421+AndreaBustos30@users.noreply.github.com>
2023-11-22 20:21:11 -0500

    Delete archivo csv con todas las cartas de una baraja de póquer estándar.R

--- archivo csv con todas las cartas de una baraja de póquer estándar.R
+++
@@ -1,9 +0,0 @@
-# Crear una baraja de póquer estándar
-suits <- c("Corazones", "Diamantes", "Tréboles", "Picas")
-values <- c("2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A")
-
-# Crear un data frame que contenga todas las cartas
-all_cards <- expand.grid(Suit = suits, Value = values)
-
-# Guardar el data frame en un archivo CSV
-write.csv(all_cards, "baraja_poker.csv", row.names = FALSE)
